7cd4fc864c1600b2fa0b2cd668747838974eda2c
Rodrigo Paucar Curasco <021100720a@uandina.edu.pe>
2026-02-26 00:47:54 -0500

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,13 +1,29 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
+
 public class Client {
     public static void main(String[] args) {
         try {
-            // Usaremos esta IP fija para el servidor mas adelante
+            // IP de la red interna
             Socket socket = new Socket("192.168.10.1", 2020);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            System.out.println("Servidor dice: " + in.readLine());
+            System.out.println("Successful connection to the server.");
+
+            BufferedReader in_socket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out_socket = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+            Scanner keyboard = new Scanner(System.in);
+
+            // Leer saludo del servidor
+            String message = in_socket.readLine();
+            System.out.println("Server says: " + message);
+
+            // Escribir nombre por teclado
+            System.out.print("Enter your name: ");
+            String myName = keyboard.nextLine();
+            out_socket.println(myName);
+
             socket.close();
+            System.out.println("Socket closed.");
         } catch (Exception e) { System.out.println("Error de conexion."); }
     }
 }
