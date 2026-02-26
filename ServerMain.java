8d1937c9b470986a2bfade5521315fdb04d30158
Rodrigo Paucar Curasco <021100720a@uandina.edu.pe>
2026-02-26 00:47:12 -0500

    Update ServerMain.java

--- ServerMain.java
+++ ServerMain.java
@@ -1,15 +1,23 @@
-import java.net.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerMain {
     private int clientnumber = 1;
+
     public ServerMain() throws Exception {
         ServerSocket server_socket = new ServerSocket(2020);
-        System.out.println("Servidor iniciado en el puerto 2020...");
+        System.out.println("Port 2020 is now open.");
         while(true) {
             Socket socket = server_socket.accept();
-            new Thread(new ServerThread(socket, this)).start();
+            ServerThread server_thread = new ServerThread(socket, this); 
+            new Thread(server_thread).start();
         }
     }
-    public synchronized int getClientNumber() { return clientnumber++; }
+
+    public synchronized int getClientNumber() {
+        return clientnumber++; 
+    }
+
     public static void main(String[] args) {
         try { new ServerMain(); } catch (Exception e) { e.printStackTrace(); }
     }
